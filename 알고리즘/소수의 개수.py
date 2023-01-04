1979a1d175174773ec83083ed73656bdd913043d
김지영 <zirzirlove96@github.com>
2023-01-04 22:23:59 +0900

    에라토스테네스 채 update

--- 알고리즘/소수의 개수.py
+++ 알고리즘/소수의 개수.py
@@ -2,7 +2,8 @@ import sys
 
 N = int(input())
 
-cnt = 0
+#1초 제한시간을 넘길 것 같음 
+"""cnt = 0
 cnt2 = 0
 for i in range(1, N+1):
     cnt = 0
@@ -13,3 +14,16 @@ for i in range(1, N+1):
         cnt2+=1
 
 print(cnt2)    
+"""
+cnt = 0
+arr = [0 for i in range(N+1)]
+
+#에라토스테네스 체 = 채로 걸러내는 방식 
+#배열로 2의 배수, 3의 배수에 1을 넣어 소수를 걸러낸다.
+for i in range(2, N+1):
+    if arr[i] == 0:
+        cnt+=1
+        for j in range(i,N+1,i):
+            arr[j]=1
+
+print(cnt)
